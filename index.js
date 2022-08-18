d50aaa4d3147ab16e253e8e9bf5091598c3ebe37
Josh Carrion <joshcarrion92@gmail.com>
2022-08-18 00:15:47 -0400

    commit tree 42f0842dd29117edfe054fe3d158349ca5b27a66

---
+++ index.js
@@ -0,0 +1,41 @@
+let number = 10;
+
+number++;
+
+number;
+
+number--;
+
+number;
+
+function add(a, b) {
+return a + b;
+}
+
+function subtract(a, b) {
+return a - b;
+}
+
+function multiply(a, b) {
+return a * b;
+}
+
+function divide(a, b) {
+return a / b;
+}
+
+function increment(number) {
+return number + 1;
+}
+
+function decrement(number) {
+    return number - 1;  
+}
+
+function makeInt(n) {
+    return parseInt(n, 10);
+}
+
+function preserveDecimal(n) {
+    return parseFloat(n);
+}
